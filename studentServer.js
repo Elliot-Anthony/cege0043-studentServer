6c137efd60a11dff1e088ec73e1d3092036508fd
Elliot Anthony <ucesecb@ucl.ac.uk>
2019-03-09 14:11:04 -0500

    Adding test file

--- studentServer.js
+++ studentServer.js
@@ -8,7 +8,19 @@ var app = express();
 // directly called in a typed URL
 var http = require('http');
 var httpServer = http.createServer(app);
-	httpServer.listen(4480);
-	app.get('/',function (req,res) {
-	res.send("hello world from the HTTP server");
-}); 
+httpServer.listen(4480);
+
+app.get('/test.html', function (req, res) {
+ // run some server-side code
+	console.log('test.html requested');
+ // note that __dirname gives the path to the studentServer.js file
+ 	res.sendFile(__dirname + '/test.html');
+});
+
+// adding functionality to log the requests
+ app.use(function (req, res, next) {
+ var filename = path.basename(req.url);
+ var extension = path.extname(filename);
+ 	console.log("The file " + filename + " was requested.");
+ 	next();
+ }); 
