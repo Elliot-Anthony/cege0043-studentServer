3dba318f67a05c4585d8dfe15dedf363c0c3213d
Elliot Anthony <ucesecb@ucl.ac.uk>
2019-03-12 14:45:23 -0400

    Adding updated studentserver

--- studentServer.js
+++ studentServer.js
@@ -6,14 +6,21 @@ var app = express();
 // add an http server to serve files to the Edge browser
 // due to certificate issues it rejects the https files if they are not
 // directly called in a typed URL
+
 var http = require('http');
 var httpServer = http.createServer(app);
 httpServer.listen(4480);
 
+var bodyParser = require('body-parser');
+app.use(bodyParser.urlencoded({
+	extended: true
+}));
+app.use(bodyParser.json());
+
 app.use(function(req, res, next) {
- res.header("Access-Control-Allow-Origin", "*");
- res.header("Access-Control-Allow-Headers", "X-Requested-With");
- next();
+ 	res.header("Access-Control-Allow-Origin", "*");
+ 	res.header("Access-Control-Allow-Headers", "X-Requested-With");
+ 	next();
  }); 
 
 // adding functionality to log the requests
@@ -23,6 +30,40 @@ app.use(function(req, res, next) {
  	console.log("The file " + filename + " was requested.");
  	next();
  }); 
+
+var fs = require('fs');
+var pg = require('pg');
+
+var configtext = ""+fs.readFileSync("/home/studentuser/certs/postGISConnection.js");
+
+// now convert the configruation file into the correct format
+var configarray = configtext.split(","); // comma as delimiter
+var config = {};
+for (var i = 0; i < configarray.length; i++) {
+	var split = configarray[i].split(':');
+	config[split[0].trim()] = split[1].trim();
+}
+
+var pool = new pg.Pool(config); 
+
+app.get('/postgistest', function (req,res) {
+	pool.connect(function(err,client,done) {
+ 		if(err){
+ 		//log, and send error to client
+ 			console.log("not able to get connection "+ err); 
+ 			res.status(400).send(err);
+ 		}
+		client.query('SELECT name FROM london_poi' ,function(err,result) {
+		done();
+ 		if(err){
+ 			console.log(err);
+ 			res.status(400).send(err);
+ 		}
+ 		res.status(200).send(result.rows);
+		});
+	});
+}); 
+
 
 // serve static files - e.g. html, css
 // this should always be the last line in the server file
